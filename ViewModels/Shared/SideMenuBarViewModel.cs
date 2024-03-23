05fa1d3eed0fb81549fdf91c90ee7835cf4c92ef
JM1F <joe7business@gmail.com>
2024-03-23 03:30:42 +0000

    Added additional menu tab and page, formatted all pages.

--- ViewModels/Shared/SideMenuBarViewModel.cs
+++ ViewModels/Shared/SideMenuBarViewModel.cs
@@ -6,49 +6,56 @@ using MahApps.Metro.IconPacks;
 namespace dev_flow.ViewModels.Shared;
 
 public class SideMenuBarViewModel : ViewModelBase
-{  
+{
     public ObservableCollection<MenuItem> Menu { get; } = new();
 
-        public ObservableCollection<MenuItem> OptionsMenu { get; } = new();
+    public ObservableCollection<MenuItem> OptionsMenu { get; } = new();
 
-        public SideMenuBarViewModel()
+    public SideMenuBarViewModel()
+    {
+        Menu.Add(new MenuItem()
         {
-            Menu.Add(new MenuItem()
-            {
-                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.HomeSolid },
-                Label = "Home",
-                NavigationType = typeof(HomePageView),
-                NavigationDestination = new Uri("Views/HomePageView.xaml", UriKind.RelativeOrAbsolute)
-            });
-            Menu.Add(new MenuItem()
-            {
-                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.TasksSolid },
-                Label = "Tasks",
-                NavigationType = typeof(KanbanPageView),
-                NavigationDestination = new Uri("Views/KanbanPageView.xaml", UriKind.RelativeOrAbsolute)
-            });
-            Menu.Add(new MenuItem()
-            {
-                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CodeBranchSolid },
-                Label = "Source Control",
-                NavigationType = typeof(SourceControlPageView),
-                NavigationDestination = new Uri("Views/SourceControlPageView.xaml", UriKind.RelativeOrAbsolute)
-            });
+            Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.HomeSolid },
+            Label = "Home",
+            NavigationType = typeof(HomePageView),
+            NavigationDestination = new Uri("Views/HomePageView.xaml", UriKind.RelativeOrAbsolute)
+        });
+        Menu.Add(new MenuItem()
+        {
+            Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.TasksSolid },
+            Label = "Tasks",
+            NavigationType = typeof(KanbanPageView),
+            NavigationDestination = new Uri("Views/KanbanPageView.xaml", UriKind.RelativeOrAbsolute)
+        });
+        Menu.Add(new MenuItem()
+        {
+            Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CodeSolid },
+            Label = "Code Sandbox",
+            NavigationType = typeof(SourceControlPageView),
+            NavigationDestination = new Uri("Views/CodeSandboxPageView.xaml", UriKind.RelativeOrAbsolute)
+        });
+        Menu.Add(new MenuItem()
+        {
+            Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CodeBranchSolid },
+            Label = "Source Control",
+            NavigationType = typeof(SourceControlPageView),
+            NavigationDestination = new Uri("Views/SourceControlPageView.xaml", UriKind.RelativeOrAbsolute)
+        });
 
 
-            OptionsMenu.Add(new MenuItem()
-            {
-                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CogsSolid },
-                Label = "Settings",
-                NavigationType = typeof(SettingsPageView),
-                NavigationDestination = new Uri("Views/SettingsPageView.xaml", UriKind.RelativeOrAbsolute)
-            });
-            OptionsMenu.Add(new MenuItem()
-            {
-                Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.InfoCircleSolid },
-                Label = "About",
-                NavigationType = typeof(AboutPageView),
-                NavigationDestination = new Uri("Views/AboutPageView.xaml", UriKind.RelativeOrAbsolute)
-            });
-        }
+        OptionsMenu.Add(new MenuItem()
+        {
+            Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.CogsSolid },
+            Label = "Settings",
+            NavigationType = typeof(SettingsPageView),
+            NavigationDestination = new Uri("Views/SettingsPageView.xaml", UriKind.RelativeOrAbsolute)
+        });
+        OptionsMenu.Add(new MenuItem()
+        {
+            Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.InfoCircleSolid },
+            Label = "About",
+            NavigationType = typeof(AboutPageView),
+            NavigationDestination = new Uri("Views/AboutPageView.xaml", UriKind.RelativeOrAbsolute)
+        });
+    }
 }
---
+++ Views/GeneralDisplayBoxControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace dev_flow.Views;
+
+public partial class GeneralDisplayBoxControl : UserControl
+{
+    public GeneralDisplayBoxControl()
+    {
+        InitializeComponent();
+    }
+    
+    public string Title
+    {
+        get => TitleTextBlock.Text;
+        set => TitleTextBlock.Text = value;
+    }
+    
+}
