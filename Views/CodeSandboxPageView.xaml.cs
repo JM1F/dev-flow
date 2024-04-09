80aac9ac1605bfcd5309a9cf0d2fd65d7f01c089
JM1F <joe7business@gmail.com>
2024-04-09 14:47:03 +0100

    Locally embedded the monaco editor in the code sandbox page.

--- Views/CodeSandboxPageView.xaml.cs
+++ Views/CodeSandboxPageView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Core;
 
 namespace dev_flow.Views;
 
@@ -10,5 +14,13 @@ public partial class CodeSandboxPageView : UserControl
     public CodeSandboxPageView()
     {
         InitializeComponent();
+    }
+
+    private void CodeSandboxPageView_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        CodeEditorWebView.Source =
+            new Uri(System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                @"Monaco\index.html"));
     }
 }
